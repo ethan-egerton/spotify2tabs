7483095e988be6c1c9e85c4dbf051a99aa49d107
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-24 19:53:42 +0000

    fix: wrap script in function

--- spotify2tabs.js
+++ spotify2tabs.js
@@ -1,17 +1,24 @@
-const songPlaying = document.querySelector('[data-testid=context-item-link]').textContent;
-const artist = document.querySelector('[data-testid=context-item-info-artist]').textContent;
-let searchQuery = artist.concat('%20', songPlaying).replace(/ /g, "%20");
+// This logic HAS to be in a function, addon simply doesnt work if its not.
+// something stupid to do with firefox extensions.
 
-if (searchQuery.includes('(')) {
-    searchQuery = searchQuery.slice(0, searchQuery.indexOf('(')); 
-}
-if (searchQuery.includes('-')) {
-    searchQuery = searchQuery.slice(0, searchQuery.indexOf('-')); 
-}
-if (searchQuery.includes('&')) {
-    searchQuery = searchQuery.replace(/&/g, "and");
+function script() {
+    const songPlaying = document.querySelector('[data-testid=context-item-link]').textContent;
+    const artist = document.querySelector('[data-testid=context-item-info-artist]').textContent;
+    let searchQuery = artist.concat('%20', songPlaying).replace(/ /g, "%20");
+
+    if (searchQuery.includes('(')) {
+        searchQuery = searchQuery.slice(0, searchQuery.indexOf('(')); 
+    }
+    if (searchQuery.includes('-')) {
+        searchQuery = searchQuery.slice(0, searchQuery.indexOf('-')); 
+    }
+    if (searchQuery.includes('&')) {
+        searchQuery = searchQuery.replace(/&/g, "and");
+    }
+
+    const url = `https://www.ultimate-guitar.com/search.php?search_type=title&value=${searchQuery}`;
+
+    window.open(url);    
 }
 
-const url = `https://www.ultimate-guitar.com/search.php?search_type=title&value=${searchQuery}`;
-
-window.open(url);
+script();

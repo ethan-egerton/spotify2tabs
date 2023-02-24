dbd163b9217d2375894bc845fc919e04b3ab388d
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-24 15:31:19 +0000

    fix: icon deactivation

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 let flag = false;
 
+const deactivatedIcon = {
+    path: {
+        "48": "deactivated_icon.png"
+    }
+}
+
+const activatedIcon = {
+    path: {
+        "48": "activated_icon.png"
+    }
+}
+
 browser.browserAction.onClicked.addListener((tab) => {
-  if (flag) {
-    browser.tabs.executeScript({file: "/spotify2tabs.js"});
-  }
+    if (flag) {
+        browser.tabs.executeScript({file: "/spotify2tabs.js"});
+    }
 });
 
-browser.tabs.onActivated.addListener((tab) => {
-  const url = tab.url;
-  if (!url.includes('https://open.spotify.com/')) {
-    browser.action.setIcon("/deactivated_icon.png");
-    flag = false;
-  } else {
-    browser.action.setIcon("/activated_icon.png");
-    flag = true;
-  }
+browser.tabs.onActivated.addListener((activeInfo) => {
+    browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const tab = tabs[0];
+        const url = tab.url;
+        console.log(url);
+        if (!url.includes('https://open.spotify.com/')) {
+            browser.browserAction.setIcon({tabId: activeInfo.tabId, ...deactivatedIcon});
+            flag = false;
+        } else {
+            browser.browserAction.setIcon({tabId: activeInfo.tabId, ...activatedIcon});
+            flag = true;
+        }
+    })
 })

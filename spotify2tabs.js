2081f5f9fb57bd00d94fc77f63f425532e216e7a
Ethan Egerton <38867954+ethan-egerton@users.noreply.github.com>
2023-02-24 19:16:37 +0000

    style: take code out function "script"

--- spotify2tabs.js
+++ spotify2tabs.js
@@ -1,20 +1,17 @@
-function script() {
-    const songPlaying = document.querySelector('[data-testid=context-item-link]').textContent;
-    const artist = document.querySelector('[data-testid=context-item-info-artist]').textContent;
-    let searchQuery = artist.concat('%20', songPlaying).replace(/ /g, "%20");
+const songPlaying = document.querySelector('[data-testid=context-item-link]').textContent;
+const artist = document.querySelector('[data-testid=context-item-info-artist]').textContent;
+let searchQuery = artist.concat('%20', songPlaying).replace(/ /g, "%20");
 
-    if (searchQuery.includes('(')) {
-        searchQuery = searchQuery.slice(0, searchQuery.indexOf('(')); 
-    }
-    if (searchQuery.includes('-')) {
-        searchQuery = searchQuery.slice(0, searchQuery.indexOf('-')); 
-    }
-    if (searchQuery.includes('&')) {
-        searchQuery = searchQuery.replace(/&/g, "and");
-    }
-
-    const url = `https://www.ultimate-guitar.com/search.php?search_type=title&value=${searchQuery}`;
-
-    window.open(url);
+if (searchQuery.includes('(')) {
+    searchQuery = searchQuery.slice(0, searchQuery.indexOf('(')); 
 }
-script();
+if (searchQuery.includes('-')) {
+    searchQuery = searchQuery.slice(0, searchQuery.indexOf('-')); 
+}
+if (searchQuery.includes('&')) {
+    searchQuery = searchQuery.replace(/&/g, "and");
+}
+
+const url = `https://www.ultimate-guitar.com/search.php?search_type=title&value=${searchQuery}`;
+
+window.open(url);

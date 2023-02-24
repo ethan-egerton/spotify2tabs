376599f2ff164d059461eaef23649ba7338ca3a1
Ethan Egerton <egertonethan@gmail.com>
2023-02-24 14:00:33 +0000

    feat: add icon deactivation

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+let flag = false;
+
 browser.browserAction.onClicked.addListener((tab) => {
+  if (flag) {
     browser.tabs.executeScript({file: "/spotify2tabs.js"});
+  }
 });
+
+browser.tabs.onActivated.addListener((tab) => {
+  const url = tab.url;
+  if (!url.includes('https://open.spotify.com/')) {
+    browser.action.setIcon("/deactivated_icon.png");
+    flag = false;
+  } else {
+    browser.action.setIcon("/activated_icon.png");
+    flag = true;
+  }
+})
